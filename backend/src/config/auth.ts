a5bffecf42a7956b1c848bef4ddd06e0918cd3b7
PedroLucasSS <pedro.lucas4431@gmail.com>
2020-07-08 18:38:47 -0300

    Backend da aplicação

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '294e074561aafd9279593c68576670b3',
+        expiresIn: '1d',
+    },
+};
---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    public async execute({
+        email,
+        password,
+    }: Request): Promise<{ user: User; token: string }> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new AppError('Incorrect email/password combination', 401);
+        }
+
+        const passwordMethod = await compare(password, user.password);
+
+        if (!passwordMethod) {
+            throw new Error('Incorrect email/password combination');
+        }
+
+        const token = sign({}, authConfig.jwt.secret, {
+            subject: user.id,
+            expiresIn: authConfig.jwt.expiresIn,
+        });
+
+        return {
+            user,
+            token,
+        };
+    }
+}
+
+export default AuthenticateUserService;
