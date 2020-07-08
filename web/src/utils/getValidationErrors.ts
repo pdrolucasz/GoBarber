dc57e1b90a47f92e47a5ba6e6d3658d6dd04f76d
PedroLucasSS <pedro.lucas4431@gmail.com>
2020-07-08 18:39:52 -0300

    Frontend da aplicação

---
+++ web/src/utils/getValidationErrors.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from 'yup'
+
+interface ValidationErros {
+    [ key: string ]: string
+}
+
+export default function getValidationErrors(err: ValidationError): ValidationErros{
+    const validationErros: ValidationErros = {}
+
+    err.inner.forEach(error => {
+        validationErros[error.path] = error.message
+    })
+
+    return validationErros
+}
