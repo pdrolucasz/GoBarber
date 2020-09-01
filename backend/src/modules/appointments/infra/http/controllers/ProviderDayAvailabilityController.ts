3edbb7741b8575e9c9160fef7e5bc40429bd4896
PedroLucasSS <pedro.lucas4431@@gmail.com>
2020-09-01 14:35:56 -0300

    ajustes na api

--- backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -10,7 +10,7 @@ export default class ProviderMonthAvailabilityController {
         response: Response,
     ): Promise<Response> {
         const { provider_id } = request.params;
-        const { month, year, day } = request.body;
+        const { month, year, day } = request.query;
 
         const listProviderDayAvailability = container.resolve(
             ListProviderDayAvailabilityService,
@@ -18,9 +18,9 @@ export default class ProviderMonthAvailabilityController {
 
         const providers = await listProviderDayAvailability.execute({
             provider_id,
-            month,
-            year,
-            day,
+            day: Number(day),
+            month: Number(month),
+            year: Number(year),
         });
 
         return response.json(providers);
--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,10 @@ import IFindAllInDayProviderDTO from '../dtos/IFindAllInDayProviderDTO';
 
 export default interface IAppointmentsRepository {
     create(data: ICrateAppointmentDTO): Promise<Appointment>;
-    findByDate(date: Date): Promise<Appointment | undefined>;
+    findByDate(
+        date: Date,
+        provider_id: string,
+    ): Promise<Appointment | undefined>;
     findAllInMonthFromProvider(
         data: IFindAllInMonthProviderDTO,
     ): Promise<Appointment[]>;
